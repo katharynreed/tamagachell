241ea93bb2737436498fd77714b45bd0f1e83f69
Clayton Collins and Katharyn Reed <clayton+katharyn@>
2017-02-07 16:44:01 -0800

    add initial php

---
+++ src/tamagatchi.php
@@ -0,0 +1,99 @@
+<?php
+
+    class Tamagatchi
+    {
+      private $hunger;
+      private $tiredness;
+      private $fun;
+      private $affection;
+      private $timer;
+
+      function __construct()
+      {
+        $this->hunger = 100;
+        $this->tiredness = 100;
+        $this->fun = 100;
+        $this->affection = 0;
+        $this->timer= 100;
+      }
+
+      function getHunger()
+      {
+        return $this->hunger;
+      }
+
+      function setHunger($hunger)
+      {
+        $this->hunger=$hunger;
+      }
+
+      function getTiredness()
+      {
+        return $this->tiredness;
+      }
+
+      function setTiredness($tiredness)
+      {
+        $this->tiredness=$tiredness;
+      }
+
+      function getFun()
+      {
+        return $this->fun;
+      }
+
+      function setFun($fun)
+      {
+        $this->fun=$fun;
+      }
+
+      function getAffection()
+      {
+        return $this->affection;
+      }
+
+      function setAffection($affection)
+      {
+        $this->affection=$affection;
+      }
+
+      function getTimer()
+      {
+        return $this->timer;
+      }
+
+      function setTimer()
+      {
+        $this->timer=time();
+      }
+
+      function timeRemaining()
+      {
+          return time() - $this->timer;
+      }
+
+      function timeDecay()
+      {
+          if (timeRemaining() < 100) {
+            return 'alive';
+          } else if (timeRemaining() => 100 ) {
+            return 'dead';
+          }
+      }
+
+      static function save()
+      {
+          array_push($_SESSION['tamagatchi_array'], $this);
+      }
+
+
+      // static function timer ()
+      // {
+      //     we don't know how to do this yet -- send help
+      // }
+
+    }
+
+
+
+ ?>
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+    $website = require_once __DIR__."/../apps/apps.php";
+    $website->run();
+
+?>
